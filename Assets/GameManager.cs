405dbe7e3fea4612c5925bf0da4f3bf56dfc926b
Emily Yu <eyudeveloper@gmail.com>
2022-02-16 09:04:09 -0800

    adding disable/activate gravity

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Physics.gravity = Vector3.zero; // init no gravity
     }
 
     // Update is called once per frame
@@ -20,6 +20,10 @@ public class GameManager : MonoBehaviour
             Vector3 worldPoint = Camera.main.ScreenToWorldPoint(relPosition);
             Vector3 adjustZ = new Vector3(worldPoint.x, worldPoint.y, enemy.transform.position.z);
             Instantiate(enemy).transform.position = adjustZ;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Space");
+            Physics.gravity = new Vector3(0, -10, 0); // reset to standard gravity
         }
     }
 }
